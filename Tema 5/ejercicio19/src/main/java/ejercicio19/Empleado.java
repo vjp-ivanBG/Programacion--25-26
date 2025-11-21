e842b16653306f7eef967f29729c063815ce41ac
vjp-ivanbg <ibuesog01@informatica.iesvalledeljerteplasencia.es>
2025-11-21 20:15:44 +0100

    ejercicio19 empezado

---
+++ Tema 5/ejercicio19/src/main/java/ejercicio19/Empleado.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio19;
+
+/**
+ *
+ * @author alumno
+ */
+public class Empleado {
+    private String Juan;
+    private String Pepe;
+    private String Saul;
+    
+    public Empleado(){
+        this.Juan = "";
+        this.Pepe = "";
+        this.Saul = "";
+    }
+    
+    public void getEmpleados() {
+        
+    }
+}
